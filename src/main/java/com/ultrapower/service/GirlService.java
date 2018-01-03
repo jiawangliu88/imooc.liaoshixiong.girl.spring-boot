120b5e0bd1750f10caea9c452739db73a98e8241
jiawangliu88 <jiawangonline@163.com>
2018-01-03 18:35:27 +0800

    新建

---
+++ src/main/java/com/ultrapower/service/GirlService.java
@@ -0,0 +1,31 @@
+package com.ultrapower.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ultrapower.Girl;
+import com.ultrapower.GirlRepository;
+
+@Service
+public class GirlService {
+
+	@Autowired
+	private GirlRepository girlRepository;
+	
+	@Transactional
+	public void inertTwo() {
+		
+		Girl girl1 = new Girl();
+		girl1.setAge(14);
+		girl1.setCupSize("B");
+		
+		Girl girl2 = new Girl();
+		girl2.setAge(15);
+		girl2.setCupSize("BB");
+		
+		girlRepository.save(girl1);
+		girlRepository.save(girl2);
+	}
+}
